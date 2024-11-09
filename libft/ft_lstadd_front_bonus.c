68c4a6dd5969dd9b8a1354fab7d93ae488c131c6
MoulatiMehdi <moulati.mehdi@gmail.com>
2024-11-09 19:26:21 +0100

    update files

--- libft/ft_lstadd_front_bonus.c
+++ libft/ft_lstadd_front_bonus.c
@@ -6,7 +6,11 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 19:47:33 by mmoulati          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2024/11/06 15:55:56 by mmoulati         ###   ########.fr       */
+=======
+/*   Updated: 2024/11/09 18:03:55 by mmoulati         ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
--- libft/ft_lstclear_bonus.c
+++ libft/ft_lstclear_bonus.c
@@ -6,7 +6,11 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 19:51:48 by mmoulati          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2024/11/06 15:58:45 by mmoulati         ###   ########.fr       */
+=======
+/*   Updated: 2024/11/09 18:04:54 by mmoulati         ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -23,4 +27,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 		ft_lstdelone(*lst, del);
 		*lst = p;
 	}
+<<<<<<< Updated upstream
+=======
+	*lst = 0;
+>>>>>>> Stashed changes
 }
